048eb6dc5f1d54b53c16d15362faece22683d0b7
Lucas Solcher <l.solcher@gmail.com>
2019-07-07 19:06:48 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      param1: 0,
+      param2: 0
+    };
+
+    this.function1 = this.function1.bind(this);
+    this.function2 = this.function2.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+
+  }
+
+
+  handleChange(event) {
+    this.setState(
+      { param1: event.target.value }
+    );
+  }
+
+
+
+
+
+
+  function1() {
+    console.log('Click happened');
+  }
+
+
+  function2() {
+    const var1 = 1;
+    const var2 = 2;
+
+    alert(var1 + var2)
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        <button onClick={this.function1}>Click Me</button>
+
+
+
+        <br></br>
+
+
+
+        <button onClick={this.function2}>Click Me Function 2</button>
+
+      </div>
+    )
+  }
 }
+
 
 export default App;
